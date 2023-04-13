10340950f84f4b19d28a1ee95ecc3f9f999feaa0
Ahmad Rizki <amrizjr@gmail.com>
2023-04-13 23:34:38 +0700

    nambah sim jalan2 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,16 @@
 package main;
 
+import java.io.*;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Loadingscreen
+        LoadFrame openingLoad = new LoadFrame(); // creates a new MyFrame object
+        delay(4000);
+        openingLoad.dispose();
+
         JFrame window = new JFrame(); // creates a new MyFrame object
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set the default close operation (exit when it is closed)
         window.setResizable(false); // set the frame to be resizable
@@ -21,5 +28,28 @@ public class Main {
     
         gamePanel.startGameThread();
 
+    }
+
+    // -------------------------------------------------------------------------------
+
+    public static void delay(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printFileContents(String fileName) {
+        String path = "data/";
+        String file = path + fileName;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
     }
 }
