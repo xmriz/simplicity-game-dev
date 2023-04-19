0de269a4771a004057134c092ac3490d40d050ab
nadinealiya <16521289@std.stei.itb.ac.id>
2023-04-19 22:57:32 +0700

    nambah benda makanan tapi belom kelar

--- src/benda/IMakanan.java
+++ src/benda/IMakanan.java
@@ -1,5 +1,22 @@
 package benda;
 
 public abstract class IMakanan extends Benda {
+    private int nilaiKekenyangan;
+    //bahan nunggu bahanmakanan
+    
+    /* nunggu bahan makanan
+    public List<BahanMakanan> getBahan(){
+        return bahan;
+    }
+    */
+
+    public int getNilaiKekenyangan(){
+        return nilaiKekenyangan;
+    }
+
+    public void setNilaiKekenyangan(int nilaiKekenyangan){
+        this.nilaiKekenyangan=nilaiKekenyangan;
+    }
+
     //test branch
 }
