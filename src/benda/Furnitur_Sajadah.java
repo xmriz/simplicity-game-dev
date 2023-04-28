4db302a5623abced4ac34e063868e0c49488c432
nadinealiya <16521289@std.stei.itb.ac.id>
2023-04-28 22:13:18 +0700

    nambah object rak buku,sajadah

---
+++ src/benda/Furnitur_Sajadah.java
@@ -0,0 +1,25 @@
+package benda;
+
+import java.awt.Rectangle;
+
+import main.GamePanel;
+
+public class Furnitur_Sajadah extends Furnitur{
+    public Furnitur_Sajadah(GamePanel gamePanel){
+        name = "Kompor Listrik";
+        image = setupImage("benda/furnitur/sajadah");
+        collision = false;
+        dimensiX = 1;
+        dimensiY = 1;
+        harga = 50;
+        aksi = "Salat";
+        solidArea = new Rectangle(0, 0, 48*dimensiX, 48*dimensiY);
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void action(){
+        // aksi masak
+        System.out.println("Aksi Salat");
+    }
+}
