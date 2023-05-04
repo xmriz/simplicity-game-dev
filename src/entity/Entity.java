a87e99418ecbef90d5fcbc8bac5a41b934682514
nadinealiya <16521289@std.stei.itb.ac.id>
2023-05-04 19:45:25 +0700

    checkpoint

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -17,27 +17,198 @@ public abstract class Entity {
 
     GamePanel gamePanel;
 
-    public int worldX, worldY;
-    public int speed;
+    private int worldX, worldY;
+    private int speed;
 
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
-    public String direction;
+    private BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    private String direction;
 
-    public int spriteCounter = 0;
-    public int spriteNumber = 1;
+    private int spriteCounter = 0;
+    private int spriteNumber = 1;
 
-    public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
-    public int solidAreaDefaultX, solidAreaDefaultY;
-    public boolean collisionOn = false;
+    private Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+    private int solidAreaDefaultX, solidAreaDefaultY;
+    private boolean collisionOn = false;
 
-    public int actionLockCounter = 0;
+    private int actionLockCounter = 0;
     int dialogCounter = 0;
 
-    public List<String> dialogs = new ArrayList<String>();
+    private List<String> dialogs = new ArrayList<String>();
 
-    public List<Benda> inventory = new ArrayList<>();
-    public int maxInventorySize = 33;
+    private List<Benda> inventory = new ArrayList<>();
+    private int maxInventorySize = 33;
 
+    public int getMaxInventorySize(){
+        return maxInventorySize;
+    }
+
+    public void setMaxInventorySize(int maxInventorySize){
+        this.maxInventorySize=maxInventorySize;
+    }
+    public List<Benda> getInventory(){
+        return inventory;
+    }
+
+    public void setInventory(List<Benda> inventory){
+        this.inventory=inventory;
+    }
+
+    public List<String> getDialogs(){
+        return dialogs;
+    }
+
+    public void setDialogs(List<String> dialogs){
+        this.dialogs=dialogs;
+    }
+
+    public int getActionLoctCounter(){
+        return actionLockCounter;
+    }
+
+    public void setActionLoctCounter(int actionLockCounter){
+        this.actionLockCounter=actionLockCounter;
+    }
+    public boolean getCollisionOn(){
+        return collisionOn;
+    }
+
+    public void setCollisionOn(boolean collisionOn){
+        this.collisionOn=collisionOn;
+    }
+    public int getSolidAreaDefaultX(){
+        return solidAreaDefaultX;
+    }
+
+    public void setSolidAreaDefaultX(int solidAreaDefaultX){
+        this.solidAreaDefaultX=solidAreaDefaultX;
+    }
+
+    public int getSolidAreaDefaultY(){
+        return solidAreaDefaultY;
+    }
+
+    public void setSolidAreaDefaultY(int solidAreaDefaultY){
+        this.solidAreaDefaultY=solidAreaDefaultY;
+    }
+    public Rectangle getSolidArea(){
+        return solidArea;
+    }
+
+    public void setSolidArea(Rectangle solidArea){
+        this.solidArea=solidArea;
+    }
+
+    public int getSpriteNumber(){
+        return spriteNumber;
+    }
+
+    public void setSpriteNumber(int spriteNumber){
+        this.spriteNumber=spriteNumber;
+    }
+    
+    public int getSpriteCounter(){
+        return spriteCounter;
+    }
+
+    public void setSpriteCounter(int spriteCounter){
+        this.spriteCounter=spriteCounter;
+    }
+    
+    public String getDirection(){
+        return direction;
+    }
+
+    public void setDirection(String direction){
+        this.direction=direction;
+    }
+    
+    public int getWorldX(){
+        return worldX;
+    }
+
+    public void setWorldX(int worldX){
+        this.worldX=worldX;
+    }
+
+    public int getWorldY(){
+        return worldY;
+    }
+
+    public void setWorldY(int worldY){
+        this.worldY=worldY;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void setSpeed(int speed){
+        this.speed=speed;
+    }
+
+    public BufferedImage getUp1(){
+        return up1;
+    }
+
+    public void setUp1(BufferedImage up1){
+        this.up1=up1;
+    }
+       
+    public BufferedImage getUp2(){
+        return up2;
+    }
+
+    public void setUp2(BufferedImage up2){
+        this.up2=up2;
+    }
+
+    public BufferedImage getDown1(){
+        return down1;
+    }
+
+    public void setDown1(BufferedImage down1){
+        this.down1=down1;
+    }
+       
+    public BufferedImage getDown2(){
+        return down2;
+    }
+
+    public void setDown2(BufferedImage down2){
+        this.down2=down2;
+    }
+
+    public BufferedImage getLeft1(){
+        return left1;
+    }
+
+    public void setLeft1(BufferedImage left1){
+        this.left1=left1;
+    }
+       
+    public BufferedImage getLeft2(){
+        return left2;
+    }
+
+    public void setLeft2(BufferedImage left2){
+        this.left2=left2;
+    }
+
+    public BufferedImage getRight1(){
+        return right1;
+    }
+
+    public void setRight1(BufferedImage right1){
+        this.right1=right1;
+    }
+       
+    public BufferedImage getRight2(){
+        return right2;
+    }
+
+    public void setRight2(BufferedImage right2){
+        this.right2=right2;
+    }
     public Entity(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
     }
@@ -58,7 +229,7 @@ public abstract class Entity {
         }
 
         // ganti direction sim
-        switch (gamePanel.listSim.get(gamePanel.indexCurrentSim).direction) {
+        switch (gamePanel.listSim.get(gamePanel.indexCurrentSim).getDirection()) {
             case "up":
                 direction = "down";
                 break;
